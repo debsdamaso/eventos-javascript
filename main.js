88942503f54021d92df8e766a45bb5692ce39dca
debsdamaso <deboracert@gmail.com>
2023-09-22 13:00:21 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+
+export function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
+
+export function setupCounterMinus(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter - 1))
+  setCounter(0)
+}
+
+setupCounter(document.querySelector('#counter'))
+setupCounterMinus(document.querySelector('#counterminus'))
+
